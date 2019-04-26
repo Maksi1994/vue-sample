0533d8d9074b0736178ef7d1c4a8e77546db8dd1
Maxim Karpinka <mak55755@gmail.com>
2019-04-26 19:08:06 +0300

    init

---
+++ src/modules/backend/Backend.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <button class="m-3" v-if="!editionIndex"
+                @click="toggleAddingItem">Add new one
+        </button>
+        <list v-if="!editionIndex && !addingItem" :items="elementsList"></list>
+        <edit v-if="editionIndex" :item="elementsList[editionIndex]"></edit>
+        <add-form v-if="addingItem"></add-form>
+    </div>
+</template>
+
+<script>
+    import List from './List.vue';
+    import AddForm from './Add-Form.vue';
+    import Edit from './Edit.vue';
+
+
+    export default {
+        data: function () {
+            return {
+                editionIndex: null,
+                addingItem: false,
+                elementsList: [
+                    {
+                        id: 1,
+                        title: 'Item Title 1',
+                        body: 'Item Body 1'
+                    },
+                    {
+                        id: 2,
+                        title: 'Item Title 2',
+                        body: 'Item Body 1'
+                    }
+                ]
+            };
+        },
+        components: {
+            List,
+            AddForm,
+            Edit
+        },
+        methods: {
+            openEdition(index) {
+                this.editionIndex = index;
+            },
+            toggleAddingItem() {
+                this.addingItem = true;
+            }
+        }
+    }
+</script>
---
+++ src/modules/backend/List.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <item v-for="item in items" :item="item" :key="item.id"></item>
+    </div>
+
+</template>
+
+<script>
+    import Item from './Item.vue';
+
+    export default {
+        name: '',
+        props: ['items'],
+        components: {
+            Item
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
